c45cd6f8c6b219a2d0b7f9c27b42c12d7f9b0d2b
Julian <julianlaxamana123@gmail.com>
2025-03-22 00:38:13 +0000

    added message things

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
-a.out: main.cpp
-	g++ main.cpp -llgpio -lSDL2
+a.out: main.o thruster.o controller.o
+	g++ main.o -llgpio -lSDL2
+
+main.o: main.cpp
+	g++ -c main.cpp
+
+thruster.o: thruster.cpp
+	g++ -c thruster.cpp
+
+controller.o: controller.cpp
+	g++ -c controller.cpp
+
+clean:
+	rm *.o
--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 #include <SDL2/SDL.h>
 #include <lgpio.h>
 #include <thread>
+#include "controller.h"
 using namespace std;
 
 const int THRUSTER1 = 26;
+const int THRUSTER2 = 6;
+const int THRUSTER3 = 5;
+const int THRUSTER4 = 0;
+
 const int LFLAGS = 0;
 
-double power = 0.0;
+double power = 0.5;
+bool thing = true;
 
 void controls(SDL_Joystick* controller){
 	SDL_Event event;
 	while (1){
 		while (SDL_PollEvent(&event)){
-			if (event.type == SDL_QUIT) break;
+			if (event.type == SDL_QUIT) thing = false;
 		}
 		// Get x axis
 		Sint16 axisX = SDL_JoystickGetAxis(controller, 0);
@@ -30,36 +36,47 @@ void controls(SDL_Joystick* controller){
 int main(){
 	int h;
 	h = lgGpiochipOpen(0); // Initialize GPIO
-	SDL_Init(SDL_INIT_JOYSTICK); // Initialize SDL
+	//SDL_Init(SDL_INIT_JOYSTICK); // Initialize SDL
 
 	// Check if Joystick is connected
-	if (SDL_NumJoysticks() < 1) {
-		std::cerr << "No joysticks connected!" << std::endl;
-		SDL_Quit();
-		return -1;
-	}
+//	if (SDL_NumJoysticks() < 1) {
+//		std::cerr << "No joysticks connected!" << std::endl;
+//		SDL_Quit();
+//		return -1;
+//	}
 
-	SDL_Joystick* controller = SDL_JoystickOpen(0); // Get the controller
+	//SDL_Joystick* controller = SDL_JoystickOpen(0); // Get the controller
 
-	thread control(controls, controller); // Start controller thread
+	//thread control(controls, controller); // Start controller thread
 
 	if (lgGpioClaimOutput(h, LFLAGS, THRUSTER1, 0) == LG_OKAY)
 	{
+		cout << "initial signal" << endl;
 		// Inital PWM output
-		for (int i = 0; i < 500; i++){
+		for (int i = 0; i < 100000; i++){
+			lgTxServo(h, THRUSTER3, 1500, 50, 0, 0); /* 1500 microseconds, 50 Hz */
 			lgTxServo(h, THRUSTER1, 1500, 50, 0, 0); /* 1500 microseconds, 50 Hz */
+			lgTxServo(h, THRUSTER2, 1500, 50, 0, 0); /* 1500 microseconds, 50 Hz */
+			lgTxServo(h, THRUSTER4, 1500, 50, 0, 0); /* 1500 microseconds, 50 Hz */
 		}
+
+		usleep(1000000);
+
 
 		// Send controller signal to thruster
-		while(1){
+		while(thing){
+			lgTxServo(h, THRUSTER3, 1500 + power * 400, 50, 0, 0); /* 1500 microseconds, 50 Hz */
 			lgTxServo(h, THRUSTER1, 1500 + power * 400, 50, 0, 0); /* 1500 microseconds, 50 Hz */
+			lgTxServo(h, THRUSTER2, 1500 + power * 400, 50, 0, 0); /* 1500 microseconds, 50 Hz */
+			lgTxServo(h, THRUSTER4, 1500 + power * 400, 50, 0, 0); /* 1500 microseconds, 50 Hz */
 		}
-	}
+	} else cout << "bruh" << endl;
 
 	// Join Thread
-	control.join();
+//	control.join();
+//
+//	SDL_JoystickClose(controller);
+//	SDL_Quit();
 
-	SDL_Quit();
-	SDL_JoystickClose(controller);
 	lgGpiochipClose(h);
 }
